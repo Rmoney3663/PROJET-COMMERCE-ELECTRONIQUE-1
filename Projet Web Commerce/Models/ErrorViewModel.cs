87d6091acc9d09516773be98f931d7a13aade0d9
Rmoney3663 <75226404+Rmoney3663@users.noreply.github.com>
2024-01-29 11:51:19 -0500

    f

---
+++ Projet Web Commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet_Web_Commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
