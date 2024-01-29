69ebf4ee443eb93b66a42fd2afde4378811fc2c2
Rmoney3663 <75226404+Rmoney3663@users.noreply.github.com>
2024-01-29 10:43:52 -0500

    created vs project

---
+++ ProjetIntegration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetIntegration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
