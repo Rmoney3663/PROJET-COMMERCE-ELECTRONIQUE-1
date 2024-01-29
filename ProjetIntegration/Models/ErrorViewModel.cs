cf9c449654111af2629cd6752b2db9884678de9a
Rmoney3663 <75226404+Rmoney3663@users.noreply.github.com>
2024-01-29 10:49:52 -0500

    Revert "created vs project"
    This reverts commit 69ebf4ee443eb93b66a42fd2afde4378811fc2c2.

--- ProjetIntegration/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjetIntegration.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
