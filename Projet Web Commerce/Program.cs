b8ff7fad9988c5dab3747222efd03abdd9f6c5e7
williamb0 <132154926+williamb0@users.noreply.github.com>
2024-01-31 16:46:11 -0500

    fix gestionnaire bug

--- Projet Web Commerce/Program.cs
+++ Projet Web Commerce/Program.cs
@@ -69,6 +69,8 @@ using (var scope = app.Services.CreateScope())
         {
             UserName = emailToCheck,
             Email = emailToCheck,
+            EmailConfirmed = true,
+
         };
 
         var result = await userManager.CreateAsync(user, "Password1!");
