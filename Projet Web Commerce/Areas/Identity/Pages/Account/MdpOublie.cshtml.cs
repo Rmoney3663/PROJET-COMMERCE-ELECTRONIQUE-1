6724b955edfffdff2bbe7c55f16026e92db983c4
Just1813 <joost.perron@gmail.com>
2024-01-31 17:48:39 -0500

    Update MdpOublie.cshtml.cs

--- Projet Web Commerce/Areas/Identity/Pages/Account/MdpOublie.cshtml.cs
+++ Projet Web Commerce/Areas/Identity/Pages/Account/MdpOublie.cshtml.cs
@@ -10,16 +10,20 @@ using Projet_Web_Commerce.Areas.Identity.Data;
 using System.ComponentModel.DataAnnotations;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using static Org.BouncyCastle.Crypto.Engines.SM2Engine;
+using Projet_Web_Commerce.Models;
+using Projet_Web_Commerce.Data;
 
 namespace Projet_Web_Commerce.Areas.Identity.Pages.Account
 {
     public class MdpOublieModel : PageModel
     {
+        private readonly AuthDbContext _context;
         private readonly UserManager<Utilisateur> _userManager;
         private readonly IEmailSender _emailSender;
 
-        public MdpOublieModel(UserManager<Utilisateur> userManager, IEmailSender emailSender)
+        public MdpOublieModel(AuthDbContext context, UserManager<Utilisateur> userManager, IEmailSender emailSender)
         {
+            _context = context;
             _userManager = userManager;
             _emailSender = emailSender;
         }
@@ -43,10 +47,29 @@ namespace Projet_Web_Commerce.Areas.Identity.Pages.Account
                 if (user == null)
                     ModelState.AddModelError(string.Empty, "Cette adresse courriel n'existe pas dans la base de données.");
 
-                //var mdpDecode = 
+                var role = "help";
+                var email = "";
+                if (User.IsInRole("Gestionnaire"))
+                    role = "Gestionnaire";
+                else if (User.IsInRole("Vendeur"))
+                {
+                    role = "Vendeur";
+                    var lstVendeurs = _context.PPVendeurs.ToList();
+                    email = (from vendeur in lstVendeurs
+                                  where vendeur.AdresseEmail == Input.Email
+                                  select vendeur).FirstOrDefault().ToString();
+                }
+                else
+                {
+                    role = "Client";
+                    var lstClients = _context.PPClients.ToList();
+                    email = (from client in lstClients
+                             where client.AdresseEmail == Input.Email
+                             select client).FirstOrDefault().ToString();
+                }
 
                 await Methodes.envoyerCourriel(Input.Email, "Oubli du mot de passe",
-                        $"Voici le mot de passe pour l'utilisateur {user.UserName}: {"chien"}");
+                        $"Voici le mot de passe pour l'utilisateur {user.UserName}: {email}");
             }
 
             // If we got this far, something failed, redisplay form
