35bdce65408e7f7a3ac85a8db31c6dcc91e416eb
williamb0 <132154926+williamb0@users.noreply.github.com>
2024-02-01 16:09:39 -0500

    carousel test + séparer gestion vendeurs et catalogue

--- Projet Web Commerce/Program.cs
+++ Projet Web Commerce/Program.cs
@@ -35,7 +35,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=MainMenu}/{action=Catalogue}");
 
 app.MapRazorPages();
 
