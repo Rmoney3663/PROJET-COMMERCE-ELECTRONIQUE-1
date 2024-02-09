4323422c48174fc333e5bb992e5934c7e647d244
williamb0 <william.anthony.burgess@gmail.com>
2024-02-09 15:34:45 -0500

    awd

--- Projet Web Commerce/Program.cs
+++ Projet Web Commerce/Program.cs
@@ -23,19 +23,11 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseDeveloperExceptionPage();
-//}
-
-app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
-
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
