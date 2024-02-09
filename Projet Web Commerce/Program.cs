d72ecb0a3c0a14bb7e31e6ade24aee640a420b09
williamb0 <william.anthony.burgess@gmail.com>
2024-02-09 14:08:38 -0500

    sAd

--- Projet Web Commerce/Program.cs
+++ Projet Web Commerce/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddControllersWithViews();
 
 
 var app = builder.Build();
-app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
+//app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
 // Configure the HTTP request pipeline.
 
 if (!app.Environment.IsDevelopment())
