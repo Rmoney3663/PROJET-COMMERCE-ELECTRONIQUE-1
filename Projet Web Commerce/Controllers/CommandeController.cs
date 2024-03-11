aedccc5837af42866024d2c7808f67ab05e85246
williamb0 <132154926+williamb0@users.noreply.github.com>
2024-03-11 09:57:50 -0400

    a

--- Projet Web Commerce/Controllers/CommandeController.cs
+++ Projet Web Commerce/Controllers/CommandeController.cs
@@ -705,7 +705,7 @@ namespace Projet_Web_Commerce.Controllers
                     { "DateExpirationCarteCredit", $"{model.dateExpiration}" },
                     { "MontantPaiement",prixtotString },
                     { "NoSecuriteCarteCredit", $"{model.CVV}" },
-                    { "NomPageRetour", $"https://localhost:5091/Commande/ConfirmerCommande"},
+                    { "NomPageRetour", $"https://localhost:44376/Commande/ConfirmerCommande"},
                     { "InfoSuppl", "Coucou" }
                     // Add other form fields as needed
                 };
